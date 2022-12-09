9141f29014341c9f7d5f6e28c6d5a38ce6bceace
Lukas Batema <112420897+lukas-batema-student@users.noreply.github.com>
2022-12-09 17:24:12 +0000

    Update Daily Logic History

--- index.js
+++ index.js
@@ -20,9 +20,20 @@ function M12D08Y2022() {
   console.log();
 }
 
+function M12D09Y2022() {
+  console.log(`${c.Bright("12/09/2022")}`);
+  console.log(`${c.Bright(c.BgRed((`Q1: ${Math.round(((4.5 / 0.8) * (100 + 15) / 100) * 100) / 100}     `)))}`); // Solves to the MBps, not the GBps
+  console.log(`${c.Bright(c.BgGreen((`Q2: ${Math.round(((39 / (50 * 3) * 100) * 100) / 100) / 100}     `)))}`); // rounds to the first two decimal places
+  console.log(`${c.Bright(c.BgGreen((`Q3: ${Math.round(((0.1536 / 3) * 24) * 10000) / 10000}   `)))}`); // rounds to the first two decimal places
+  console.log(`${c.Bright(c.BgGreen((`Q4: ${Math.round(((1.3 * 5) + (1.5 * 12)) * 10) / 10}     `)))}`); // rounds to one place out, gets answer in MB, not GB
+  console.log(`${c.Bright(c.BgGreen((`Q5: ${Math.round((((((15.50 * (5.6 + 3.4 + 7.1 + 8.0 + 0)) + ((15.50 * 1.5) * (7.7 + 4.0)))) * 100) * 100) / 100) / 100}   `)))}`); // rounds two places out
+  console.log();
+}
+
 function allDailyLogic() {
   M12D07Y2022();
   M12D08Y2022();
+  M12D09Y2022();
 }
 
 allDailyLogic();
