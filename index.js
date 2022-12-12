da0feb8dac85df477da93b08ac64d691c25191da
Lukas Batema <112420897+lukas-batema-student@users.noreply.github.com>
2022-12-12 18:25:21 +0000

    Q1: C, Q2: C, Q3: C, Q4: F, Q5: F

--- index.js
+++ index.js
@@ -22,11 +22,21 @@ function M12D08Y2022() {
 
 function M12D09Y2022() {
   console.log(`${c.Bright("12/09/2022")}`);
-  console.log(`${c.Bright(c.BgRed((`Q1: ${Math.round(((4.5 / 0.8) * (100 + 15) / 100) * 100) / 100}     `)))}`); // Solves to the MBps, not the GBps
-  console.log(`${c.Bright(c.BgGreen((`Q2: ${Math.round(((39 / (50 * 3) * 100) * 100) / 100) / 100}     `)))}`); // rounds to the first two decimal places
-  console.log(`${c.Bright(c.BgGreen((`Q3: ${Math.round(((0.1536 / 3) * 24) * 10000) / 10000}   `)))}`); // rounds to the first two decimal places
-  console.log(`${c.Bright(c.BgGreen((`Q4: ${Math.round(((1.3 * 5) + (1.5 * 12)) * 10) / 10}     `)))}`); // rounds to one place out, gets answer in MB, not GB
-  console.log(`${c.Bright(c.BgGreen((`Q5: ${Math.round((((((15.50 * (5.6 + 3.4 + 7.1 + 8.0 + 0)) + ((15.50 * 1.5) * (7.7 + 4.0)))) * 100) * 100) / 100) / 100}   `)))}`); // rounds two places out
+  console.log(`${c.Bright(c.BgRed((`Q1: ${Math.round(((4.5 / 0.8) * (100 + 15) / 100) * 100) / 100}     `)))}`);
+  console.log(`${c.Bright(c.BgGreen((`Q2: ${Math.round(((39 / (50 * 3) * 100) * 100) / 100) / 100}     `)))}`);
+  console.log(`${c.Bright(c.BgGreen((`Q3: ${Math.round(((0.1536 / 3) * 24) * 10000) / 10000}   `)))}`);
+  console.log(`${c.Bright(c.BgGreen((`Q4: ${Math.round(((1.3 * 5) + (1.5 * 12)) * 10) / 10}     `)))}`);
+  console.log(`${c.Bright(c.BgGreen((`Q5: ${Math.round((((((15.50 * (5.6 + 3.4 + 7.1 + 8.0 + 0)) + ((15.50 * 1.5) * (7.7 + 4.0)))) * 100) * 100) / 100) / 100}   `)))}`);
+  console.log();
+}
+
+function M12D12Y2022() {
+  console.log(`${c.Bright("12/12/2022")}`);
+  console.log(`${c.Bright("Q1:")} ${c.BgGreen(100 - ((120 / 150) * 100) + "%      ")}`);
+  console.log(`${c.Bright("Q2:")} ${c.BgGreen(Math.round((((2 / 45) * 100) * 100) / 10) / 10 + "%     ")}`);
+  console.log(`${c.Bright("Q3:")} ${c.BgGreen(Math.round((67.43 * 1.25) * 100) / 100 + "    ")}`);
+  console.log(`${c.Bright("Q4:")} ${c.BgRed((320 * 0.25) + "       ")}`);
+  console.log(`${c.Bright("Q5:")} ${c.BgRed(Math.round((975.00 * 1.04) * 100) / 100 + "     ")}`);
   console.log();
 }
 
@@ -34,6 +44,7 @@ function allDailyLogic() {
   M12D07Y2022();
   M12D08Y2022();
   M12D09Y2022();
+  M12D12Y2022();
 }
 
 allDailyLogic();
